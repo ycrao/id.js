f1e5a3920767de2cb7c178137beaa4579734f787
raoyc <raoyc2009@gmail.com>
2018-05-18 10:33:12 +0800

    update test.js

--- example/test.js
+++ example/test.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-var Id_1 = require("../src/Id");
+var Id_1 = require("../index");
 var id = new Id_1.IdentityCard('42032319930606629x');
 console.log('identity card number: 42032319930606629x');
 console.log('result: %j', id.info());
